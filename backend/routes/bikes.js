71d7944faa2485914f1e0e5f1931d8c751eb5993
JMorvid-05 <juandavidmayorgaac@gmail.com>
2025-11-11 01:21:07 -0500

    Ajuste de código

--- backend/routes/bikes.js
+++ backend/routes/bikes.js
@@ -4,7 +4,7 @@ import * as bicicletaController from "../controllers/bikeController.js";
 const router = Router();
 
 router.get("/", bicicletaController.getBicicletas);
-router.get("/tipo/:tipo", bicicletaController.getBicicletasPorTipo);
+router.get("/Bicicletatipo/", bicicletaController.getBicicletasPorTipo);
 router.get("/:id/telemetria", bicicletaController.getTelemetriaActual);
 router.get("/:id/telemetria/historico", bicicletaController.getTelemetriaHistorico);
 
