0029268d57760a37737ca9b9e9654031f9489226
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-11-20 08:11:49 -0500

    comunicaión y simulación para dispositivos iot y broker mqtt

---
+++ backend/iot/server-side.js
@@ -0,0 +1,21 @@
+import * as bikeController from "../controllers/bikeController.js"
+import { bicicletaService } from "../services/bike/bike.services.js";
+import { bikeHandler } from "../services/bike/bike-handler.js";
+
+const mqtt = require('mqtt');
+export const client = mqtt.connect('mqtt://localhost:1883');
+
+client.on('connect', () => {
+  console.log("Successful connection to the mqtt broker");
+  client.subscribe('bikes/+/telemetry', async (data) => {
+    await bicicletaService.registrarTelemetria(data);
+  });
+  client.subscribe('bikes/+/status', async (id, status) => {
+	await bikeHandler.changeStatus(id, status);
+  });
+});
+
+client.on('message', (topic, buffer) => {
+  console.log(`Message from ${topic}`);
+  
+});
