2643caaf3543968b81109ec6dadc6470534728cd
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-11-20 13:09:21 -0500

    ahora inicializa el cliente mqtt

--- backend/server.js
+++ backend/server.js
@@ -95,6 +95,9 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "Ruta no encontrada" });
 });
 
+import { initMqttClient } from "./iot/server-side.js";
+initMqttClient();
+
 // ==================== 🚀 INICIAR SERVIDOR ====================
 
 app.listen(PORT, () => {
