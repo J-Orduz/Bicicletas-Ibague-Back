52ead85b4267ddc637a74978c44cb9bf1c158bd5
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-11-25 21:04:24 -0500

    .

--- backend/iot/server-side.js
+++ backend/iot/server-side.js
@@ -8,19 +8,21 @@ const mqtt = require('mqtt');
 
 // asume que el servidor broker se ejecuta en el mismo dispositivo que el
 // servidor principal
-const client = mqtt.connect('mqtt://localhost:1883');
+//const client = mqtt.connect('mqtt://localhost:1883');
+const client = mqtt.connect({
+  port: 1883, host: `192.168.56.1`, keepalive: 90});
 
 export function initMqttClient() {
   console.log("[MQTT] Conectandose al broker mqtt...");
   client.on('connect', () => {
-    console.log("Successful connection to the mqtt broker");
+    console.log("[MQTT] Conexion al broker exitosa");
 
     // el dispositivo se suscribe a los canales despues de conectarse exitosamente al broker
     client.subscribe('bikes/+/telemetry', async (data) => {
       await bicicletaService.registrarTelemetria(data);
     });
     client.subscribe('bikes/+/status', async (id, status) => {
-    await bikeHandler.changeStatus(id, status);
+      await bikeHandler.changeStatus(id, status);
     });
   });
 
