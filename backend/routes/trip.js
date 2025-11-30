326a2cc5a93218b0d676ecf431cd30a11d85c8c8
J-Orduz <juanorduz011@gmail.com>
2025-11-30 03:31:57 -0500

    Add Descuento por puntos

--- backend/routes/trip.js
+++ backend/routes/trip.js
@@ -8,5 +8,7 @@ router.post("/finalizar", tripController.finalizarViajeAuth);
 router.get("/viajes", tripController.getViajesAuth);
 router.post("/pagoExito",tripController.setPagoViajeExitosoAuth);
 
+router.post("/canjear-puntos", tripController.canjearPuntosDescuentoAuth);
+
 
 export default router;
